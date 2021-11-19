06840fefed3d40195d438376a6bb6a73b7a3f8e3
ASPB <alindabyamukama@gmail.com>
2021-11-19 19:51:39 +0300

    completed :Module3

--- demo.js
+++ demo.js
@@ -2,10 +2,39 @@
 (function() {
 
   let person = {
-    firstName: 'Susan',
-    lastName: 'Alinda',
-    age: 28,
+    name: {
+      first: 'Susan',
+      last: 'Alinda'
+    },
+    age: 28
   };
-  display(person);
+  // what if we want to know this persons full name?
+
+  Object.defineProperty(person, 'fullName', 
+  {
+    get: function() {
+      return this.name.first + ' ' + this.name.last;
+      },
+      // a set function thats going to set proprties on our person object
+      set: function(value) {
+        var nameParts = value.split(' ');
+        this.name.first = nameParts[0];
+        this.name.last = nameParts[1];
+      }
+
+  });
+// now we can change a persons full name using the set function
+
+  person.fullName = 'firstName lastName';
+
+  display(person.fullName);
+  display(person.name.first);
+  display(person.name.last);
+
 
 })();
+
+/*
+getters and setters!
+
+*/
