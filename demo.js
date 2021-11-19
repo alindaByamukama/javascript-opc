8deeea81fec8b154124ab51f7f7e425d0f4bd13c
ASPB <alindabyamukama@gmail.com>
2021-11-19 21:44:49 +0300

    notes:Module

--- demo.js
+++ demo.js
@@ -1,40 +1,15 @@
 'use strict'; 
 (function() {
 
-  let person = {
-    name: {
-      first: 'Susan',
-      last: 'Alinda'
-    },
-    age: 28
-  };
-  // what if we want to know this persons full name?
-
-  Object.defineProperty(person, 'fullName', 
-  {
-    get: function() {
-      return this.name.first + ' ' + this.name.last;
-      },
-      // a set function thats going to set proprties on our person object
-      set: function(value) {
-        var nameParts = value.split(' ');
-        this.name.first = nameParts[0];
-        this.name.last = nameParts[1];
-      }
-
-  });
-// now we can change a persons full name using the set function
-
-  person.fullName = 'firstName lastName';
-
-  display(person.fullName);
-  display(person.name.first);
-  display(person.name.last);
-
+  function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
 })();
 
 /*
-getters and setters!
+instance versus prototype properties
+
 
 */
